497663ddbce60bd64e17dc8daf4926a7a05e2d3b
Rafael Campos <racampos@gmail.com>
2023-11-21 16:08:49 -0600

    Adds the discord prover

--- tlsn/zk_notary/src/main.rs
+++ tlsn/zk_notary/src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpServer};
+mod discord_prover;
 mod simple_prover;
 
 #[actix_web::main]
@@ -6,7 +7,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/notarize_simple", web::get().to(simple_prover::notarize))
-            .route("/placeholder", web::get().to(simple_prover::notarize))
+            .route("/notarize_discord", web::get().to(discord_prover::notarize))
     })
     .bind("127.0.0.1:3000")?
     .run()
