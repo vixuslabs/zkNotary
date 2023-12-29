9c9c008dc1ee6743221ff57402feea823d2cb178
Rafael Campos <racampos@gmail.com>
2023-12-29 09:29:54 -0600

    Adds a twitter-specific parser from the 'zknotary-parsers' npm package.

--- examples/node/index.js
+++ examples/node/index.js
@@ -1,4 +1,5 @@
 import * as wasm from "zknotary-verifier";
+import { twitter } from "zknotary-parsers";
 import fs from 'fs/promises';
 
 async function readFile(file) {
@@ -15,7 +16,9 @@ const notary_pubkey = await readFile('./notary.pub');
 
 try {
     const result = wasm.verify(proof_json, notary_pubkey);
-    console.log(result);
+    // console.log(result);
+    const parsedData = twitter.parse(result);
+    console.log(JSON.stringify(parsedData, null, 2));
 } catch (e) {
     console.error("Verification failed:", e);
 }
