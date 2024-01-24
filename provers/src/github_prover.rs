705c21d8054ea24a7c64ad32cae6a01191b07882
Rafael Campos <racampos@gmail.com>
2024-01-24 09:29:54 -0600

    Adds a new Github prover, meant to be used to retrieve all commits for a given user in a given repo during a certain period of time.

---
+++ provers/src/github_prover.rs
@@ -0,0 +1,354 @@
+use actix_web::{web, HttpResponse, Responder};
+use eyre::Result;
+use futures::AsyncWriteExt;
+use hyper::{body::to_bytes, client::conn::Parts, Body, Request, StatusCode};
+use rustls::{Certificate, ClientConfig, RootCertStore};
+use serde::{Deserialize, Serialize};
+use std::{env, error::Error, fs::File as StdFile, io::BufReader, ops::Range, sync::Arc};
+use tlsn_core::proof::TlsProof;
+use tokio::{fs::File, net::TcpStream};
+use tokio_rustls::TlsConnector;
+use tokio_util::compat::{FuturesAsyncReadCompatExt, TokioAsyncReadCompatExt};
+use tracing::debug;
+use url::Url;
+
+use tlsn_prover::tls::{Prover, ProverConfig};
+
+// Setting of the application server
+const SERVER_DOMAIN: &str = "api.github.com";
+const ROUTE: &str = "repos";
+
+// Setting of the notary server
+const NOTARY_HOST: &str = "127.0.0.1";
+const NOTARY_PORT: u16 = 7047;
+const NOTARY_CA_CERT_PATH: &str = "./src/rootCA.crt";
+
+// Configuration of notarization
+const NOTARY_MAX_TRANSCRIPT_SIZE: usize = 16384;
+
+/// Response object of the /session API
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NotarizationSessionResponse {
+    pub session_id: String,
+}
+
+/// Request object of the /session API
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NotarizationSessionRequest {
+    pub client_type: ClientType,
+    /// Maximum transcript size in bytes
+    pub max_transcript_size: Option<usize>,
+}
+
+/// Types of client that the prover is using
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum ClientType {
+    /// Client that has access to the transport layer
+    Tcp,
+    /// Client that cannot directly access transport layer, e.g. browser extension
+    Websocket,
+}
+
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    username: String,
+    repo: String,
+    since: String,
+    until: String
+}
+
+pub async fn notarize(query_params: web::Query<QueryParams>) -> impl Responder {
+    let username = &query_params.username;
+    let repo = &query_params.repo;
+    let since = &query_params.since;
+    let until = &query_params.until;
+
+    tracing_subscriber::fmt::init();
+
+    // Load secret variables frome environment for twitter server connection
+    dotenv::dotenv().ok();
+    let bearer_token = env::var("GITHUB_BEARER_TOKEN").unwrap();
+
+    let (notary_tls_socket, session_id) = setup_notary_connection().await;
+
+    // Basic default prover config using the session_id returned from /session endpoint just now
+    let config = ProverConfig::builder()
+        .id(session_id)
+        .server_dns(SERVER_DOMAIN)
+        .build()
+        .unwrap();
+
+    // Create a new prover and set up the MPC backend.
+    let prover = Prover::new(config)
+        .setup(notary_tls_socket.compat())
+        .await
+        .unwrap();
+
+    let client_socket = tokio::net::TcpStream::connect((SERVER_DOMAIN, 443))
+        .await
+        .unwrap();
+
+    println!("Connected to the Notary");
+
+    // Bind the Prover to server connection
+    let (tls_connection, prover_fut) = prover.connect(client_socket.compat()).await.unwrap();
+
+    // Spawn the Prover to be run concurrently
+    let prover_task = tokio::spawn(prover_fut);
+
+    // Attach the hyper HTTP client to the TLS connection
+    let (mut request_sender, connection) = hyper::client::conn::handshake(tls_connection.compat())
+        .await
+        .unwrap();
+
+    // Spawn the HTTP task to be run concurrently
+    let connection_task = tokio::spawn(connection.without_shutdown());
+
+    // Build the HTTP request to fetch the tweet
+    let request = Request::builder()
+        // .uri(format!(
+        //     "https://{}/{}/{}/{}/commits?since={}&until={}&page=1",
+        //     SERVER_DOMAIN,
+        //     ROUTE,
+        //     username,
+        //     repo,
+        //     since,
+        //     until
+        // ))
+        .uri(format!("https://api.github.com/repos/racampos/zkNotary/commits?since=2024-01-01T00:00:00Z&until=2024-01-03T23:59:59Z&page=1"))
+        // .header("Host", SERVER_DOMAIN)
+        // .header("Connection", "close")
+        // .header("Authorization", format!("Bearer {bearer_token}"))
+        .body(Body::empty())
+        .unwrap();
+
+    debug!("Sending request");
+    println!("Starting an MPC TLS connection with the Github server");
+
+    let response = request_sender.send_request(request).await.unwrap();
+
+    debug!("Sent request");
+
+    assert!(response.status() == StatusCode::OK);
+
+    debug!("Request OK");
+    println!("Got a response from the Github server");
+
+    // Pretty printing :)
+    let payload = to_bytes(response.into_body()).await.unwrap().to_vec();
+    let parsed =
+        serde_json::from_str::<serde_json::Value>(&String::from_utf8_lossy(&payload)).unwrap();
+    debug!("{}", serde_json::to_string_pretty(&parsed).unwrap());
+
+    // Close the connection to the server
+    let mut client_socket = connection_task.await.unwrap().unwrap().io.into_inner();
+    client_socket.close().await.unwrap();
+    print!("Closed the connection to the Github server");
+
+    // The Prover task should be done now, so we can grab it.
+    let prover = prover_task.await.unwrap().unwrap();
+
+    // Prepare for notarization
+    let mut prover = prover.start_notarize();
+
+    // Identify the ranges in the transcript that contain secrets
+    let (public_ranges, private_ranges) =
+        find_ranges(prover.sent_transcript().data(), &[bearer_token.as_bytes()]);
+
+    let recv_len = prover.recv_transcript().data().len();
+
+    let builder = prover.commitment_builder();
+
+    // Collect commitment ids for the outbound transcript
+    let mut commitment_ids = public_ranges
+        .iter()
+        .chain(private_ranges.iter())
+        .map(|range| builder.commit_sent(range.clone()).unwrap())
+        .collect::<Vec<_>>();
+
+    // Commit to the full received transcript in one shot, as we don't need to redact anything
+    commitment_ids.push(builder.commit_recv(0..recv_len).unwrap());
+
+    // Finalize, returning the notarized session
+    let notarized_session = prover.finalize().await.unwrap();
+
+    debug!("Notarization complete!");
+    println!("Notarization completed successfully!");
+
+    let session_proof = notarized_session.session_proof();
+
+    let mut proof_builder = notarized_session.data().build_substrings_proof();
+
+    // Reveal everything but the bearer token (which was assigned commitment id 2)
+    proof_builder.reveal(commitment_ids[0]).unwrap();
+    proof_builder.reveal(commitment_ids[1]).unwrap();
+    proof_builder.reveal(commitment_ids[3]).unwrap();
+
+    let substrings_proof = proof_builder.build().unwrap();
+
+    let proof = TlsProof {
+        session: session_proof,
+        substrings: substrings_proof,
+    };
+
+    let res = serde_json::json!({
+      "proof": proof,
+      "notarized_session": notarized_session
+    });
+
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(serde_json::to_string_pretty(&res).unwrap())
+}
+
+async fn setup_notary_connection() -> (tokio_rustls::client::TlsStream<TcpStream>, String) {
+    // Connect to the Notary via TLS-TCP
+    let mut certificate_file_reader = read_pem_file(NOTARY_CA_CERT_PATH).await.unwrap();
+    let mut certificates: Vec<Certificate> = rustls_pemfile::certs(&mut certificate_file_reader)
+        .unwrap()
+        .into_iter()
+        .map(Certificate)
+        .collect();
+    let certificate = certificates.remove(0);
+
+    let mut root_store = RootCertStore::empty();
+    root_store.add(&certificate).unwrap();
+
+    let client_notary_config = ClientConfig::builder()
+        .with_safe_defaults()
+        .with_root_certificates(root_store)
+        .with_no_client_auth();
+    let notary_connector = TlsConnector::from(Arc::new(client_notary_config));
+
+    let notary_socket = tokio::net::TcpStream::connect((NOTARY_HOST, NOTARY_PORT))
+        .await
+        .unwrap();
+
+    let notary_tls_socket = notary_connector
+        // Require the domain name of notary server to be the same as that in the server cert
+        .connect("tlsnotaryserver.io".try_into().unwrap(), notary_socket)
+        .await
+        .unwrap();
+
+    // Attach the hyper HTTP client to the notary TLS connection to send request to the /session endpoint to configure notarization and obtain session id
+    let (mut request_sender, connection) = hyper::client::conn::handshake(notary_tls_socket)
+        .await
+        .unwrap();
+
+    // Spawn the HTTP task to be run concurrently
+    let connection_task = tokio::spawn(connection.without_shutdown());
+
+    // Build the HTTP request to configure notarization
+    let payload = serde_json::to_string(&NotarizationSessionRequest {
+        client_type: ClientType::Tcp,
+        max_transcript_size: Some(NOTARY_MAX_TRANSCRIPT_SIZE),
+    })
+    .unwrap();
+
+    let request = Request::builder()
+        .uri(format!("https://{NOTARY_HOST}:{NOTARY_PORT}/session"))
+        .method("POST")
+        .header("Host", NOTARY_HOST)
+        // Need to specify application/json for axum to parse it as json
+        .header("Content-Type", "application/json")
+        .body(Body::from(payload))
+        .unwrap();
+
+    debug!("Sending configuration request");
+
+    let configuration_response = request_sender.send_request(request).await.unwrap();
+
+    debug!("Sent configuration request");
+
+    assert!(configuration_response.status() == StatusCode::OK);
+
+    debug!("Response OK");
+
+    // Pretty printing :)
+    let payload = to_bytes(configuration_response.into_body())
+        .await
+        .unwrap()
+        .to_vec();
+    let notarization_response =
+        serde_json::from_str::<NotarizationSessionResponse>(&String::from_utf8_lossy(&payload))
+            .unwrap();
+
+    debug!("Notarization response: {:?}", notarization_response,);
+
+    // Send notarization request via HTTP, where the underlying TCP connection will be extracted later
+    let request = Request::builder()
+        // Need to specify the session_id so that notary server knows the right configuration to use
+        // as the configuration is set in the previous HTTP call
+        .uri(format!(
+            "https://{}:{}/notarize?sessionId={}",
+            NOTARY_HOST,
+            NOTARY_PORT,
+            notarization_response.session_id.clone()
+        ))
+        .method("GET")
+        .header("Host", NOTARY_HOST)
+        .header("Connection", "Upgrade")
+        // Need to specify this upgrade header for server to extract tcp connection later
+        .header("Upgrade", "TCP")
+        .body(Body::empty())
+        .unwrap();
+
+    debug!("Sending notarization request");
+
+    let response = request_sender.send_request(request).await.unwrap();
+
+    debug!("Sent notarization request");
+
+    assert!(response.status() == StatusCode::SWITCHING_PROTOCOLS);
+
+    debug!("Switched protocol OK");
+
+    // Claim back the TLS socket after HTTP exchange is done
+    let Parts {
+        io: notary_tls_socket,
+        ..
+    } = connection_task.await.unwrap().unwrap();
+
+    (notary_tls_socket, notarization_response.session_id)
+}
+
+/// Find the ranges of the public and private parts of a sequence.
+///
+/// Returns a tuple of `(public, private)` ranges.
+fn find_ranges(seq: &[u8], sub_seq: &[&[u8]]) -> (Vec<Range<usize>>, Vec<Range<usize>>) {
+    let mut private_ranges = Vec::new();
+    for s in sub_seq {
+        for (idx, w) in seq.windows(s.len()).enumerate() {
+            if w == *s {
+                private_ranges.push(idx..(idx + w.len()));
+            }
+        }
+    }
+
+    let mut sorted_ranges = private_ranges.clone();
+    sorted_ranges.sort_by_key(|r| r.start);
+
+    let mut public_ranges = Vec::new();
+    let mut last_end = 0;
+    for r in sorted_ranges {
+        if r.start > last_end {
+            public_ranges.push(last_end..r.start);
+        }
+        last_end = r.end;
+    }
+
+    if last_end < seq.len() {
+        public_ranges.push(last_end..seq.len());
+    }
+
+    (public_ranges, private_ranges)
+}
+
+/// Read a PEM-formatted file and return its buffer reader
+async fn read_pem_file(file_path: &str) -> Result<BufReader<StdFile>> {
+    let key_file = File::open(file_path).await?.into_std().await;
+    Ok(BufReader::new(key_file))
+}
+
--- provers/src/main.rs
+++ provers/src/main.rs
@@ -2,6 +2,7 @@ use actix_web::{web, App, HttpServer};
 mod discord_prover;
 mod simple_prover;
 mod twitter_prover;
+mod github_prover;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -10,6 +11,7 @@ async fn main() -> std::io::Result<()> {
             .route("/notarize_simple", web::get().to(simple_prover::notarize))
             .route("/notarize_discord", web::get().to(discord_prover::notarize))
             .route("/notarize_twitter", web::get().to(twitter_prover::notarize))
+            .route("/notarize_github", web::get().to(github_prover::notarize))
     })
     .bind("127.0.0.1:3000")?
     .run()
