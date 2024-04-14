6ad9dda8a67989debd6f2f557f31527773fd247f
Rafael Campos <racampos@gmail.com>
2024-04-14 15:36:55 -0600

    Creates a new directory for the o1js version of the verifier and adds the relevant data structures, smart contract and tests.

---
+++ o1js-verifier/src/TlsnVerifier.ts
@@ -0,0 +1,26 @@
+import {
+  SmartContract,
+  state,
+  State,
+  method,
+  Signature,
+  PublicKey,
+} from 'o1js';
+
+import { SessionHeader } from './SessionHeader.js';
+
+export class TlsnVerifier extends SmartContract {
+  @state(PublicKey) notaryPublicKey = State<PublicKey>();
+
+  @method async verify(
+    sessionHeader: SessionHeader,
+    signature: Signature
+  ) {
+    // Get the notary public key from the contract state
+    const notaryPublicKey = this.notaryPublicKey.getAndRequireEquals();
+
+    // Evaluate whether the signature is valid for the provided data
+    const validSignature = signature.verify(notaryPublicKey, sessionHeader.toFields());
+    validSignature.assertTrue("Signature is not valid");
+  }
+}
