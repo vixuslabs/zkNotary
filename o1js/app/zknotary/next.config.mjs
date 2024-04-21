cf760d5d699d0b196faba786e2602f057d8ff946
0xtito <welovethepenguins@gmail.com>
2024-04-20 22:45:16 -0400

    remove jest + add cross headers

--- o1js/app/zknotary/next.config.mjs
+++ o1js/app/zknotary/next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, _) => {
+    config.module.rules.push({
+      test: /\.glsl/,
+      loader: "webpack-glsl-loader",
+    });
+
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+
+    return config;
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
